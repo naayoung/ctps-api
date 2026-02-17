57c9d7add22a3601af58a5dde5bd209e5a5b88f0
naayoung <ask5046a@gmail.com>
2026-02-18 00:18:17 +0900

    init

---
+++ src/main/java/com/ctps/ctps_api/CtpsApiApplication.java
@@ -0,0 +1,13 @@
+package com.ctps.ctps_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CtpsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CtpsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ctps/ctps_api/CtpsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ctps.ctps_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CtpsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
